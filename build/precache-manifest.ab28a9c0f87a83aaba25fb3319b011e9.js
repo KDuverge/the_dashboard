334b05d2b5727bc5553d3162e8c414ec7cd3fe9c
Kenneth Duverge <duvergekenneth@gmail.com>
2019-11-16 20:54:23 -0500

    build folder

---
+++ build/precache-manifest.ab28a9c0f87a83aaba25fb3319b011e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3864ae8cc41fef9c8f428ce70d8afeea",
+    "url": "/the_dashboard/index.html"
+  },
+  {
+    "revision": "789b1a90d36dd1373d99",
+    "url": "/the_dashboard/static/css/2.1d3f04dd.chunk.css"
+  },
+  {
+    "revision": "6c1e664a8fd28ad8369f",
+    "url": "/the_dashboard/static/css/main.b5f05ab4.chunk.css"
+  },
+  {
+    "revision": "789b1a90d36dd1373d99",
+    "url": "/the_dashboard/static/js/2.acce12b0.chunk.js"
+  },
+  {
+    "revision": "6c1e664a8fd28ad8369f",
+    "url": "/the_dashboard/static/js/main.28752f6c.chunk.js"
+  },
+  {
+    "revision": "ea7ee410adc32d5fd925",
+    "url": "/the_dashboard/static/js/runtime~main.69f57f8f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/the_dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/the_dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/the_dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/the_dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/the_dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/the_dashboard/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/the_dashboard/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/the_dashboard/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/the_dashboard/static/media/notification.c0d3c94c.eot"
+  }
+]);
